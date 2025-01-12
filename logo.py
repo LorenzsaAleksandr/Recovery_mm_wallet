9d8df4cbd5f07ee9d98e34854eb014adef1ea02b
lorenzsa <67155656+LorenzsaAleksandr@users.noreply.github.com>
2025-01-12 21:56:06 +0300

    Add files via upload

---
+++ logo.py
@@ -0,0 +1,24 @@
+LOGO: str = '''
+
+                                                                                                                                                                                                 dddddddd                    
+                                                                                                                                                                                                                                                                              
+lllllll                                                                                                                                
+l:::::l                                                                                                                                
+l:::::l                                                                                                                                
+l:::::l                                                                                                                                
+ l::::l    ooooooooooo   rrrrr   rrrrrrrrr       eeeeeeeeeeee    nnnn  nnnnnnnn    zzzzzzzzzzzzzzzzz    ssssssssss     aaaaaaaaaaaaa   
+ l::::l  oo:::::::::::oo r::::rrr:::::::::r    ee::::::::::::ee  n:::nn::::::::nn  z:::::::::::::::z  ss::::::::::s    a::::::::::::a  
+ l::::l o:::::::::::::::or:::::::::::::::::r  e::::::eeeee:::::een::::::::::::::nn z::::::::::::::z ss:::::::::::::s   aaaaaaaaa:::::a 
+ l::::l o:::::ooooo:::::orr::::::rrrrr::::::re::::::e     e:::::enn:::::::::::::::nzzzzzzzz::::::z  s::::::ssss:::::s           a::::a 
+ l::::l o::::o     o::::o r:::::r     r:::::re:::::::eeeee::::::e  n:::::nnnn:::::n      z::::::z    s:::::s  ssssss     aaaaaaa:::::a 
+ l::::l o::::o     o::::o r:::::r     rrrrrrre:::::::::::::::::e   n::::n    n::::n     z::::::z       s::::::s        aa::::::::::::a 
+ l::::l o::::o     o::::o r:::::r            e::::::eeeeeeeeeee    n::::n    n::::n    z::::::z           s::::::s    a::::aaaa::::::a 
+ l::::l o::::o     o::::o r:::::r            e:::::::e             n::::n    n::::n   z::::::z      ssssss   s:::::s a::::a    a:::::a 
+l::::::lo:::::ooooo:::::o r:::::r            e::::::::e            n::::n    n::::n  z::::::zzzzzzzzs:::::ssss::::::sa::::a    a:::::a 
+l::::::lo:::::::::::::::o r:::::r             e::::::::eeeeeeee    n::::n    n::::n z::::::::::::::zs::::::::::::::s a:::::aaaa::::::a 
+l::::::l oo:::::::::::oo  r:::::r              ee:::::::::::::e    n::::n    n::::nz:::::::::::::::z s:::::::::::ss   a::::::::::aa:::a
+llllllll   ooooooooooo    rrrrrrr                eeeeeeeeeeeeee    nnnnnn    nnnnnnzzzzzzzzzzzzzzzzz  sssssssssss      aaaaaaaaaa  aaaa
+                                                                                                                                       
+                                                                                                                                       
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+'''
